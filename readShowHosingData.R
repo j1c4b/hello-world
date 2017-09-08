b1d7bbb02d5778b92abc1b87ac143e340fdac72f
j1c4b <31754559+j1c4b@users.noreply.github.com>
2017-09-08 18:46:26 -0400

    Delete readShowHosingData.R

--- readShowHosingData.R
+++
@@ -1,9 +0,0 @@
-readShowHosingData <- function()
-{
-  library(ggplot2)
-housing <- read.csv("C:/Users/jg0003/Documents/R/landdata-states.csv") 
-#head(housing[1:5])
-#hist(housing$Home.Value)
-#ggplot(housing, aes(x=Home.Value)) + geom_histogram(stat_bin(aes(x=count,y=Home.Value), bins = NULL))
-ggplot(housing, aes(x=Home.Value)) + geom_histogram()
-}
