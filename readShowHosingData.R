287ceecb9b38c00233ab8829bc1b2be978778c1e
j1c4b <31754559+j1c4b@users.noreply.github.com>
2017-09-11 18:09:17 -0400

    Add files via upload
    20170911_1

--- readShowHosingData.R
+++ readShowHosingData.R
@@ -8,11 +8,12 @@ housing <- read.csv("C:/Users/jg0003/Documents/R/landdata-states.csv")
 #ggplot(housing, aes(x=Home.Value)) + geom_histogram()
 
 dev.new()
-plot(Home.Value ~ Date, data = subset(housing, State =="MA"))
+##plot(Home.Value ~ Date, data = subset(housing, State =="MA"))
+ggplot(subset(housing, State %in% c("NJ","MA")),
+             aes(x=Date,y=Home.Value,color=State)) + geom_point()
+#points(Home.Value ~ Date, col="red", data=subset(housing, State == "NJ"))
 
-points(Home.Value ~ Date, col="red", data=subset(housing, State == "NJ"))
-
-legend(1975,400000,c("MA","NJ"), title = "State", col = c("black", "red"),pch = c(1,1))
+#legend(1975,400000,c("MA","NJ"), title = "State", col = c("black", "red"),pch = c(1,1))
 
 
 }
