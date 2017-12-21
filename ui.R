dad3eeb906b6afce2c502da1bd7149cc2642f513
j1c4b <31754559+j1c4b@users.noreply.github.com>
2017-12-21 00:20:00 -0500

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,23 +1,24 @@
 library(shiny)
 
 ui <- fluidPage(
-
-
-    #Title Panel
-    titlePanel("Compute Population Mean"),
-
-    #SideBar
-    #sidebarPanel(
-    column(4,
-      wellPanel(
-      textInput("arrayNumbers", label=h3("List of Numbers"), value = ""),
-
-      h4("Sum of all elements"), verbatimTextOutput("resultSum"),
-      h4("Number of all elements"), verbatimTextOutput("resultNum"),
-      h4("Mean of all elements"), verbatimTextOutput("resultMean"),
-      h4("Medium of all elements"), verbatimTextOutput("resultMedium"),
-
-      actionButton("ComputeButton","Compute")
-              )
-          )
+  
+  
+  #Title Panel
+  titlePanel("Compute Population Mean"),
+  
+  #SideBar
+  #sidebarPanel(
+  column(4,
+         wellPanel(
+           textInput("arrayNumbers", label=h3("List of Numbers"), value = ""),
+           
+           h4("Sorted list of all elements"), verbatimTextOutput("resultSort"),
+           h4("Sum of all elements"), verbatimTextOutput("resultSum"),
+           h4("Number of all elements"), verbatimTextOutput("resultNum"),
+           h4("Mean of all elements"), verbatimTextOutput("resultMean"),
+           h4("Medium of all elements"), verbatimTextOutput("resultMedium"),
+           
+           actionButton("ComputeButton","Compute")
+         )
+  )
 )
