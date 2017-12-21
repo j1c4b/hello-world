363428c8a92e9b6fb63f9b01b08c370256a3ef2d
j1c4b <31754559+j1c4b@users.noreply.github.com>
2017-12-21 00:18:46 -0500

    Update server.R
    Added sorted

--- server.R
+++ server.R
@@ -1,39 +1,42 @@
-#x <- function(n) {sum(as.numeric(unlist(strsplit(n, split = ","))))}
-
 server <- function(input, output, session) {
-
-  nSum <- eventReactive(input$ComputeButton, {
-
-    sum(as.numeric(unlist(strsplit(input$arrayNumbers, split = ","))))
- #   function(input$arrayNumbers)
-    }
+  
+  nSort <- eventReactive(input$ComputeButton, {
+    sort.int(as.numeric(unlist(strsplit(input$arrayNumbers, split = ","))), decreasing = FALSE)}
     )
 
+  nSum <- eventReactive(input$ComputeButton, {
+    
+    sum(as.numeric(unlist(strsplit(input$arrayNumbers, split = ","))))
+    #   function(input$arrayNumbers)
+  }
+  )
+  
   nNum <- eventReactive(input$ComputeButton, {
     length(unlist(strsplit(input$arrayNumbers, split = ",")))
   })
-
+  
   nMean <- eventReactive(input$ComputeButton, {
     sum(as.numeric(unlist(strsplit(input$arrayNumbers, split = ",")))) / length(unlist(strsplit(input$arrayNumbers, split = ",")))
-
+    
   })
-
+  
   nMedium <- eventReactive(input$ComputeButton, {
     #browser()
     nArr <-c(sort.int(as.numeric(unlist(strsplit(input$arrayNumbers, split = ","))), decreasing = FALSE))
     nArrL <- length(nArr)
     nOE <- (nArrL %% 2)
-
+    
     if (nOE == 0) { nMed <- (nArr[nArrL/2] + nArr[(nArrL/2 + 1)])/2} else {nMed <- nArr[(nArrL + 1)/2]}
-
+    
     nMed
   })
-
-
+  
+  
   # You can access the value of the widget with input$text, e.g.
   output$resultSum <- renderText({ nSum()})
   output$resultNum <- renderText({ nNum()})
   output$resultMean <- renderText({ nMean()})
   output$resultMedium <- renderText({ nMedium()})
-
+  output$resultSort <- renderText({nSort()}) 
+  
 }
