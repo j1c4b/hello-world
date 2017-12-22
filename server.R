5bfea3f06cc27fef2fa02675768e1561b847220d
j1c4b <31754559+j1c4b@users.noreply.github.com>
2017-12-22 14:55:17 -0500

    Add files via upload

--- server.R
+++ server.R
@@ -1,59 +1,90 @@
 server <- function(input, output, session) {
-  
+
+
+#   observe({
+#     input$arrayNumbers
+#     if (is.null(input$arrayNumbers) == TRUE ) {input$arrayNumbers = NULL }
+#   })
+#
+# if (is.na(input$arrayNumbers) == TRUE) {x <- NULL} esle {x <- 1}
+
+  nNum <- eventReactive(input$ComputeButton, {
+    length(unlist(strsplit(input$arrayNumbers, split = ",")))
+  })
+
+
+  # if (is.null(x) == TRUE) {dt <- table(NULL,NULL)} else {
+  #
+  # nArray <- eventReactive(as.numeric(unlist(strsplit(input$arrayNumbers, split = ","))))
+  #
+  # nArrL <- length(nArray)
+  #
+  # dt <- table(nArray,nArrL)
+  # }
+
   nSort <- eventReactive(input$ComputeButton, {
     sort.int(as.numeric(unlist(strsplit(input$arrayNumbers, split = ","))), decreasing = FALSE)}
-    )
+  )
 
   nSum <- eventReactive(input$ComputeButton, {
-    
+
     sum(as.numeric(unlist(strsplit(input$arrayNumbers, split = ","))))
     #   function(input$arrayNumbers)
   }
   )
-  
-  nNum <- eventReactive(input$ComputeButton, {
-    length(unlist(strsplit(input$arrayNumbers, split = ",")))
-  })
-  
+
   nPopulationMean <- eventReactive(input$ComputeButton, {
     #sum(as.numeric(unlist(strsplit(input$arrayNumbers, split = ",")))) / length(unlist(strsplit(input$arrayNumbers, split = ",")))
     nArr <-c(as.numeric(unlist(strsplit(input$arrayNumbers, split = ","))))
     mean(nArr)
-             
+
   })
   nMedium <- eventReactive(input$ComputeButton, {
     nArr <-c(sort.int(as.numeric(unlist(strsplit(input$arrayNumbers, split = ","))), decreasing = FALSE))
     nArrL <- length(nArr)
     nOE <- (nArrL %% 2)
-    
+
     if (nOE == 0) { nMed <- (nArr[nArrL/2] + nArr[(nArrL/2 + 1)])/2} else {nMed <- nArr[(nArrL + 1)/2]}
-    
+
     nMed
   })
-  
+
   nPopulationVariance <- eventReactive(input$ComputeButton, {
     nArr <-c(as.numeric(unlist(strsplit(input$arrayNumbers, split = ","))))
     nArrL <- length(nArr)
     sum((nArr - mean(nArr))^2)/nArrL
   })
-  
+
   nPopulationStandartDeviation <- eventReactive(input$ComputeButton, {
     nArr <-c(as.numeric(unlist(strsplit(input$arrayNumbers, split = ","))))
     nArrL <- length(nArr)
     sqrt(sum((nArr - mean(nArr))^2)/nArrL)
   })
-  
-  
+
+
   # You can access the value of the widget with input$text, e.g.
   output$resultSum <- renderText({ nSum()})
   output$resultNum <- renderText({ nNum()})
   output$resultPopMean <- renderText({ nPopulationMean()})
   output$resultMedium <- renderText({ nMedium()})
-  output$resultSort <- renderText({nSort()}) 
-  output$resultPopVar <- renderText({nPopulationVariance()}) 
-  output$resultPopStdDev <- renderText({nPopulationStandartDeviation()}) 
-  
-  #output$NumPlot <- renderPlot({input$as.numeric(unlist(strsplit(input$arrayNumbers, split = ",")))),
-  
-  
+  output$resultSort <- renderText({nSort()})
+  output$resultPopVar <- renderText({nPopulationVariance()})
+  output$resultPopStdDev <- renderText({nPopulationStandartDeviation()})
+
+  browser()
+  output$NumPlot <- renderPlot({
+
+    dt <- table(NULL,NULL)
+    nArray <- eventReactive(as.numeric(unlist(strsplit(input$arrayNumbers, split = ","))))
+
+    nArrL <- length(nArray)
+
+    dt <- table(nArray,nArrL)
+
+     # plot(x=ncol(as.numeric(unlist(strsplit(input$arrayNumbers, split = ",")))),
+     #      y= as.numeric(unlist(strsplit(input$arrayNumbers, split = ","))))
+     barplot(dt,
+             width=1, main="Elements",xlab="Element Number",ylab="Elenet Value"
+             )
+    })
 }
--- ui.R
+++ ui.R
@@ -1,21 +1,21 @@
 library(shiny)
 
 ui <- fluidPage(
-  
-  
+
+
   #Title Panel
   titlePanel("Compute Population Mean"),
-  
+
   #SideBar
   #sidebarPanel(
   column(4,
          wellPanel(
-           textInput("arrayNumbers", label=h3("List of Numbers"), value = "0.3, 0.91, 0.69, 0.57, 0.28, 0.86, 0.68, 0.36, 0.83, 0.88, 
-0.76, 0.85, 1.05, 0.7, 0.83, 0.76, 0.51, 0.6, 0.8, 0.71, 1.24, 
-                     1.09, 0.23, 0.67, 0.97, 0.97, 0.37, 0.31, 0.76, 0.63, 0.91, 0.97, 
-                     0.4, 0.59, 1.05, 0.57, 0.12, 0.69, 0.86, 0.7, 0.6, 0.69, 0.51, 
+           textInput("arrayNumbers", label=h3("List of Numbers"), value = "0.3, 0.91, 0.69, 0.57, 0.28, 0.86, 0.68, 0.36, 0.83, 0.88,
+                     0.76, 0.85, 1.05, 0.7, 0.83, 0.76, 0.51, 0.6, 0.8, 0.71, 1.24,
+                     1.09, 0.23, 0.67, 0.97, 0.97, 0.37, 0.31, 0.76, 0.63, 0.91, 0.97,
+                     0.4, 0.59, 1.05, 0.57, 0.12, 0.69, 0.86, 0.7, 0.6, 0.69, 0.51,
                      0.72, 1.27, 0.82, 0.43, 0.86, 0.32, 0.83"),
-           
+    #?       conditionalPanel()
            h4("Sorted list of all elements"), verbatimTextOutput("resultSort"),
            h4("Sum of all elements"), verbatimTextOutput("resultSum"),
            h4("Number of all elements"), verbatimTextOutput("resultNum"),
@@ -24,9 +24,10 @@ ui <- fluidPage(
            h4("Population Variance of all elements"), verbatimTextOutput("resultPopVar"),
            h4("Population Standart Deviation of all elements"), verbatimTextOutput("resultPopStdDev"),
            actionButton("ComputeButton","Compute")
-         )
-  )
-  
-  #mainPanel(plotOutput("NumPlot"))
-  
+           )
+         ),
+
+  mainPanel(
+
+     plotOutput("NumPlot"))
 )
